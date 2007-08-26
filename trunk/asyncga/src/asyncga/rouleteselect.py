91c20053a66609498910a51d74f90558097639d0
girino <girino@48bd7f46-9038-0410-ac46-33526eb0987a>
2007-08-26 18:23:50 +0000

    de jong performance metrics asyncga stub

--- trunk/asyncga/src/asyncga/rouleteselect.py
+++ trunk/asyncga/src/asyncga/rouleteselect.py
@@ -7,25 +7,29 @@ from random import random
 from asyncga.numeric_individual import dejong_f2_individual
 import __main__
 
-def roulete_select(population, inverse = False):
-    """roulete selects in a population
-    """
-    fitnesses = [x.evaluate() for x in population]
-    minf = min(fitnesses)
-    # rescale fitnesses
-    fitnesses = [x - minf for x in fitnesses]
-    if inverse:
-        maxf = max(fitnesses)
-        fitnesses = [maxf - x for x in fitnesses]
-    # now it's all between 0 and max
-    sumf = sum(fitnesses)
-    maxprob = uniform(0, sumf)
-    current = 0
-    acumm = 0
-    while acumm < maxprob:
-        acumm = acumm + fitnesses[current]
-        current = current + 1
-    return population[current-1]
+class roulete:
+    def __init__(self, population, inverse=False):
+        """initializes the roulete process
+        """
+        self.population = population
+        fitnesses = [x.evaluate() for x in population]
+        minf = min(fitnesses)
+        # rescale fitnesses
+        fitnesses = [x - minf for x in fitnesses]
+        if inverse:
+            maxf = max(fitnesses)
+            fitnesses = [maxf - x for x in fitnesses]
+        # now it's all between 0 and max
+        self.fitnesses = fitnesses
+        self.sumf = sum(fitnesses)
+    def select(self):
+        maxprob = uniform(0, self.sumf)
+        current = 0
+        acumm = 0
+        while acumm < maxprob:
+            acumm = acumm + self.fitnesses[current]
+            current = current + 1
+        return self.population[current-1]
 
 class traditional_ga:
     """This class implements the traditional ga.
@@ -37,6 +41,7 @@ class traditional_ga:
         self.population = [factory_method() for i in range(0, size)]
         self.size = size
         self.mut_prob = 0.01
+        self.performance_calculator = performance_calculator()
     def run(self, maxiter, target=MAXINT, delta=0):
         """runs the GA
         """
@@ -45,23 +50,91 @@ class traditional_ga:
         best_eval = best.evaluate()
         iter = 0
         while iter < maxiter and abs(target - best_eval) > delta:
-            next_gen = [roulete_select(population, True).mate(roulete_select(population, True)) for i in range(0, self.size)]
+            selector = roulete(population, True)
+            next_gen = [selector.select().mate(selector.select()) for i in range(0, self.size)]
             for individual in next_gen:
                 if random() < self.mut_prob:
                     individual.mutate()
-            best = next_gen[0]
+            best = get_best(next_gen)
             best_eval = best.evaluate()
-            for individual in next_gen:
-                if individual.evaluate() < best_eval:
-                    best = individual
-                    best_eval = individual.evaluate()
             population = next_gen
-            print iter, best_eval, best
             iter = iter + 1
+            self.performance_calculator.update_iter([x.evaluate() for x in population])
+            # printing is last thing
+            print iter, best_eval, best, self.performance_calculator
         print iter, best_eval, best
         return best
+    
+def get_best(population):
+    best = population[0]
+    best_eval = best.evaluate()
+    for individual in population:
+        if individual.evaluate() < best_eval:
+            best = individual
+            best_eval = individual.evaluate()
+    return best
+
+class performance_calculator:
+    """Calculates dejong's online and offline performances
+    """
+    def __init__(self):
+        self.iter = 0
+        self.online_sum = 0.0
+        self.offline_sum = 0.0
+    def update_iter(self, fitnesses):
+        self.iter = self.iter + 1
+        # online
+        avg = 1.0*sum(fitnesses)/len(fitnesses)
+        self.online_sum = self.online_sum + avg
+        # offline
+        bestfit = min(fitnesses)
+        self.offline_sum = self.offline_sum + bestfit
+    def online(self):
+        return 1.0*self.online_sum/self.iter
+    def offline(self):
+        return 1.0*self.offline_sum/self.iter
+    def __repr__(self):
+        return "online: %f, offline %f" % (self.online(), self.offline())
+    
+class asyncga:
+    def __init__(self, factory_method, size, life_expectancy=None):
+        self.population = [factory_method() for i in range(0, size)]
+        self.size = size
+        self.life_expectancy = life_expectancy
+        self.mut_prob = 0.01
+        self.performance_calculator = performance_calculator()
+    def run(self, maxiter, target=MAXINT, delta=0):
+        population = self.population
+        iter = 0
+        best = get_best(population)
+        best_eval = best.evaluate()
+        while iter < maxiter and abs(target - best_eval) > delta:
+            population = self.mate(population)
+            population = self.mutate(population)
+            population = self.die(population)
+            best = get_best(population)
+            best_eval = best.evaluate()
+            self.population = population
+            iter = iter + 1
+            self.performance_calculator.update_iter([x.evaluate() for x in population])
+            # printing is last thing
+            print iter, best_eval, best, self.performance_calculator
+        return best
+    def mate(self, population):
+        # mimics the traditional GA.
+        selector = roulete(population, True)
+        return [selector.select().mate(selector.select()) for i in range(0, self.size)]
+    def mutate(self, population):
+        for individual in population:
+            if random() < self.mut_prob:
+                individual.mutate()
+        return population
+    def die(self, population):
+        # traditional GA, no one dies
+        return population
         
 
+
 if __main__:
-    ga = traditional_ga(lambda: dejong_f2_individual(), 1000)
-    ga.run(100, 0, 0.001)
+    ga = asyncga(lambda: dejong_f1_individual(), 200)
+    ga.run(100, 0, 0.0001)
